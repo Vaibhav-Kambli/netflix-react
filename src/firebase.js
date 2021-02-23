f739e4cbb4f59debc15988563c67bcaa2bc630ec
Vaibhav Kambli <vaibhavk2608@gmail.com>
2021-02-23 16:49:21 -0500

    Removed firebase config from repository

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyB_QpbE1IHz8wz-0DlYM_S5Vaaa0PdEBcU",
-    authDomain: "netflix-react-portfolio.firebaseapp.com",
-    projectId: "netflix-react-portfolio",
-    storageBucket: "netflix-react-portfolio.appspot.com",
-    messagingSenderId: "128788610268",
-    appId: "1:128788610268:web:52b08812b8ddfbf72f4819"
-  };
